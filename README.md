15b9c848de8ae370745c5ba6adb66156f50c0e79
bharadwaj-raju <bharadwaj.raju777@gmail.com>
2016-07-17 11:57:37 +0530

    Make code consistent-er, update README

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # lks-indicator
+
 Indicator to display status of lock keys (Caps, Scroll, Num) for Linux (using AppIndicator).
+
+GIF demonstration of lks-indicator:
+
+![GIF demonstration of lks-indicator](http://i.imgur.com/FxSae8c.gif)
+
+Licensed under the MIT license.
 
 ## Installation
 
@@ -8,3 +15,17 @@ Indicator to display status of lock keys (Caps, Scroll, Num) for Linux (using Ap
 - Run `lock_keys_status.py`.
 
 - Add `python /path/to/lock_keys_status.py` to system startup.
+
+## Usage
+
+When none of Scroll, Caps or Num lock are pressed, the circle will be green.
+
+When one is pressed, the circle becomes red, and the following is displayed:
+
+- *\[C\]* for *Caps Lock*
+- *\[N\]* for *Num Lock*
+- *\[S\]* for *Scroll Lock*
+
+**Exiting:**
+
+Click on the circle and choose *Quit*.
--- lock_keys_status.py
+++ lock_keys_status.py
@@ -1,28 +1,39 @@
 #!/usr/bin/env python
-#
+# coding: utf-8
+
 ###########################################################
-# Author: Serg Kolo , contact: 1047481448@qq.com 
-# Date: July 16,2012
+# Author: Serg Kolo , contact: 1047481448@qq.com
+# Date: July 16, 2012
 # Purpose: Simple indicator of Caps, Num, and Scroll Lock
-#          keys for Ubuntu
-# 
+#		   keys for Ubuntu
+#
 # Written for: http://askubuntu.com/q/796985/295286
 # Tested on: Ubuntu 16.04 LTS
-###########################################################
-# Copyright: Serg Kolo , 2016
-#    
-#     Permission to use, copy, modify, and distribute this software is hereby granted
-#     without fee, provided that  the copyright notice above and this permission statement
-#     appear in all copies.
 #
-#     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-#     IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-#     FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
-#     THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-#     LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-#     FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-#     DEALINGS IN THE SOFTWARE.
-
+###########################################################
+#
+# Licensed under The MIT License (MIT).
+# See included LICENSE file or the notice below.
+#
+# Copyright © 2016 Sergiy Kolodyazhnyy
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
 import glib
 import subprocess
@@ -32,90 +43,87 @@ import os
 
 class LockKeyStatusIndicator(object):
 
-    def __init__(self):
-             
-        self.app = appindicator.Indicator('LKS', '', 
-                          appindicator.CATEGORY_APPLICATION_STATUS)
-        
-        self.app.set_status(appindicator.STATUS_ACTIVE)
-        
-        self.update_label()
-        
-        self.app_menu = gtk.Menu()
-        self.quit_app = gtk.MenuItem('Quit')
-        self.quit_app.connect('activate', self.quit)
-        self.quit_app.show()
-        self.app_menu.append(self.quit_app)
-        
-        self.app.set_menu(self.app_menu)
+	def __init__(self):
 
-        
-    def run(self):
-       
-        try:
-            gtk.main()
-        except KeyboardInterrupt:
-            pass
+		self.app = appindicator.Indicator('LKS', '',
+						  appindicator.CATEGORY_APPLICATION_STATUS)
 
-    def quit(self, data=None):
-    
-        gtk.main_quit()
-  
-    
-    def run_cmd(self, cmdlist):
-        
-        try:
-            stdout = subprocess.check_output(cmdlist)
-        except subprocess.CalledProcessError:
-               pass
-        else:
-            if stdout:
-                return stdout
+		self.app.set_status(appindicator.STATUS_ACTIVE)
 
-    
-    def key_status(self):
-        
-        label = ''
-        status = []
-        keys = { '3':'C', '7':'N', '11':'S' }
-    
-        for line in self.run_cmd(['xset', 'q']).split('\n') :
-            if 'Caps Lock:' in line:
-                status = line.split()
-    
-        for index in 3, 7, 11:
-            if status[index] == "on" :
-               label = label + " [" +  keys[ str(index)  ] + "] " 
-            #else:
-            #   label = label + keys[ str(index) ]
-    
-        return label
+		self.update_label()
 
-    
-    def update_label(self):
-         
-        cwd = os.getcwd()
-        red_icon = os.path.join(cwd, red.png)
-        green_icon = os.path.join(cwd, 'green.png')
-        label_text = self.key_status()
-        if '[' in label_text:
-            self.app.set_icon(red_icon)
-        else:
-            self.app.set_icon(green_icon)
-         
-        self.app.set_label(label_text)
-        glib.timeout_add_seconds(1, self.set_app_label)
+		self.app_menu = gtk.Menu()
+		self.quit_app = gtk.MenuItem('Quit')
+		self.quit_app.connect('activate', self.quit)
+		self.quit_app.show()
+		self.app_menu.append(self.quit_app)
 
-    
-    def set_app_label(self):
-        
-        self.update_label()
+		self.app.set_menu(self.app_menu)
+
+
+	def run(self):
+
+		try:
+			gtk.main()
+		except KeyboardInterrupt:
+			pass
+
+	def quit(self, data=None):
+
+		gtk.main_quit()
+
+
+	def run_cmd(self, cmdlist):
+
+		try:
+			stdout = subprocess.check_output(cmdlist)
+		except subprocess.CalledProcessError:
+			   pass
+		else:
+			if stdout:
+				return stdout
+
+
+	def key_status(self):
+
+		label = ''
+		status = []
+		keys = { '3':'C', '7':'N', '11':'S' }
+
+		for line in self.run_cmd(['xset', 'q']).split('\n') :
+			if 'Caps Lock:' in line:
+				status = line.split()
+
+		for index in 3, 7, 11:
+			if status[index] == "on" :
+			   label += " [" + keys[str(index)] + "] "
+
+		return label
+
+
+	def update_label(self):
+
+		cwd = os.getcwd()
+		red_icon = os.path.join(cwd, 'red.png')
+		green_icon = os.path.join(cwd, 'green.png')
+		label_text = self.key_status()
+		if '[' in label_text:
+			self.app.set_icon(red_icon)
+		else:
+			self.app.set_icon(green_icon)
+
+		self.app.set_label(label_text)
+		glib.timeout_add_seconds(1, self.set_app_label)
+
+
+	def set_app_label(self):
+
+		self.update_label()
 
 def main():
-    
-    indicator = LockKeyStatusIndicator()
-    indicator.run()
+
+	indicator = LockKeyStatusIndicator()
+	indicator.run()
 
 if __name__ == '__main__':
-    main()
-
+	main()
